780e206323942b8ba65520d6c2c363e39b250040
Rodrigo Gonçalves <rodrigo.goncalves2001r@gmail.com>
2022-01-06 19:48:13 -0300

    add project todolist frontend

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import router from './router';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import '@/assets/css/tailwind.css';
+
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
