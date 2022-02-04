8bd346505049cae9e64bb207f4a52b0378b5acee
rodrigo-dev <rodrigo.goncalves2001r@gmail.com>
2022-02-04 00:25:38 -0300

    added spinner in project

---
+++ src/components/Spinner/Spinner.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <img
+            :src='spinner'
+            alt='spinner'
+        >
+    </div>
+</template>
+
+<script>
+    import spinner from '@/assets/img/spinner.png';
+
+    export default {
+        name: 'Spinner',
+        data(){
+            return {
+                spinner: spinner,
+            };
+        },
+    };
+</script>
