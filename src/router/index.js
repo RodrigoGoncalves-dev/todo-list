a565b0a32234549794417d711687bf1722205120
rodrigo-dev <rodrigo.goncalves2001r@gmail.com>
2022-02-08 21:04:07 -0300

    add new route and use linkActive

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,25 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Login from '../views/Login.vue';
 import RegisterCard from '@/components/Register/RegisterCard';
+import VerifyEmail from '@/views/VerifyEmail';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
+    path: '/login',
     name: 'Login',
     component: Login,
   },
   {
-    path: '/register',
+    path: '/registrar',
     name: 'RegisterCard',
     component: RegisterCard,
+  },
+  {
+    path: '/verificar-email',
+    name: 'VerifyEmail',
+    component: VerifyEmail,
   },
 ];
 
@@ -22,6 +28,7 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+  linkActiveClass: 'active',
 });
 
 export default router;
