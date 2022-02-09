e35f001b1db2267f150e43233983ced7026ddd61
rodrigo-dev <rodrigo.goncalves2001r@gmail.com>
2022-02-08 21:07:21 -0300

    verification email from new users

---
+++ src/views/VerifyEmail.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="h-screen flex items-center justify-center bg-gray-900 content-center">
+        <div
+            v-if="response.message"
+            :class="`rounded-sm bg-${response.color}-100 p-4 mb-4`"
+        >
+            <h3 :class="`text-sm leading-5 font-medium text-${response.color}-800`">
+                {{ response.message }}
+            </h3>
+        </div>
+    </div>
+</template>
+
+<script>
+    import message from '@/utils/message';
+
+    export default {
+        name: 'VerifyEmail',
+        components: {
+        },
+        data() {
+            return {
+                token: '',
+                response: {
+                    color: '',
+                    message: '',
+                },
+                spinner: {
+                    verifyEmail: false,
+                },
+            };
+        },
+        beforeRouteEnter(to, from, next) {
+            if(!to.query.token) {
+                next({ name: 'Login' });
+            }
+            next();
+        },
+        created() {
+            this.token = this.$route?.query?.token ;
+            this.verifyEmail();
+        },
+        methods: {
+            verifyEmail(){
+                this.$axios.post('v1/verify-email', { token: this.token })
+                    .then(() => {
+                        this.response.color = 'green';
+                        this.response.message = 'E-mail verficado com sucesso!';
+                    })
+                    .catch((e) => {
+                        const errorCode = e?.response?.data?.error || 'ServerError';
+                        this.response.color = 'red';
+                        this.response.message = message[errorCode];
+                    });
+            },
+        },
+    };
+</script>
