d6b42ee4b8b3e0df6b2c46ea378cd0302ed98448
rodrigo-dev <rodrigo.goncalves2001r@gmail.com>
2022-02-08 21:05:08 -0300

    using linkActive in routes and styling

--- src/components/Login/LoginMenu.vue
+++ src/components/Login/LoginMenu.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="grid grid-rows-1 space-x-0.5 justify-items-center mb-5">
         <RouterLink
-            to="/"
-            class="row-start-1 text-gray-500 text-lg font-medium active:border-b-2 active:border-blue-700 p-5"
+            to="/login"
+            active-class="row-start-1 text-gray-500 text-lg font-medium border-b-2 border-blue-700 p-5"
+            class="row-start-1 text-gray-500 text-lg font-medium p-5"
         >
             Login
         </RouterLink>
         <RouterLink
-            to="/register"
-            class="row-start-1 text-gray-500 text-lg font-medium active:border-b-2 active:border-blue-700 p-5"
+            to="/registrar"
+            active-class="row-start-1 text-gray-500 text-lg font-medium border-b-2 border-blue-700 p-5"
+            class="row-start-1 text-gray-500 text-lg font-medium p-5"
         >
             Cadastrar
         </RouterLink>
