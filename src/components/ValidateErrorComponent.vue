cdab0a12e4498a9f348c5a926528a97a635af8ee
rodrigo-dev <rodrigo.goncalves2001r@gmail.com>
2022-02-12 23:21:49 -0300

    created component error of validation

---
+++ src/components/ValidateErrorComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div
+        v-if="!!errorMessage[0]"
+        class="text-red-500 text-s mb-2"
+    >
+        {{ errorMessage[0] }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ValidateErrorComponent',
+        methods: {},
+        props: {
+            errorMessage: {
+                type: Array,
+            },
+        },
+    };
+</script>
