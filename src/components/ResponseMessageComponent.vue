2aeb4a4d31322edcafe0358a5ab2611bb81b0224
rodrigo-dev <rodrigo.goncalves2001r@gmail.com>
2022-02-12 23:37:01 -0300

    add with ResponseMessageComponent.vue

---
+++ src/components/ResponseMessageComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div
+        v-if="message"
+        :class="`rounded-sm xxxsm:w-80 sm:w-96 md:w-96 lg:w-96 xl:w-96 2xl:w-96 bg-${color}-100 p-4 mb-4`"
+    >
+        <h3 :class="`text-sm leading-5 font-medium text-${color}-800 text-left`">
+            {{ message }}
+        </h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ResponseMessageComponent',
+        props: {
+            message: {
+                type: String,
+            },
+            color: {
+                type: String,
+            },
+        },
+    };
+</script>
